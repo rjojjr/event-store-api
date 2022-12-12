0758b96577515034d6c3ab89becb26ec4e5e8cc3
kirchner <rjojjr91@live.com>
2022-12-12 15:39:47 -0600

    fix wrapper - rk

--- Models/GetEventsResponse.cs
+++ Models/GetEventsResponse.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+
 namespace event_store_api.Models
 {
 	public class GetEventsResponse
 	{
+
+        public GetEventsResponse()
+        {
+
+        }
+
         public GetEventsResponse(int resultCount, IList<GenericEventHttpModel> results)
         {
             ResultCount = resultCount;
             Results = results;
         }
 
+        [JsonPropertyName("resultCount")]
         private int ResultCount { get; set; }
 
-		private IList<GenericEventHttpModel> Results { get; set; } = new List<GenericEventHttpModel>();
+        [JsonPropertyName("results")]
+        private IList<GenericEventHttpModel> Results { get; set; } = new List<GenericEventHttpModel>();
 	}
 }
 
