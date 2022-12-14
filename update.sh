e5bd26e4745cb052472fa65c73641d887779729d
kirchner <rjojjr91@live.com>
2022-12-14 11:10:56 -0600

    add update script - rk

---
+++ update.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+git pull
+
+echo 'building bagend-event-api'
+docker build -t bagend-event-api .
+
+cd ../bagend-web-scraper
+echo 'building bagend-web-scraper'
+git pull
+docker build -t bagend-web-scraper .
+
+cd ../bagend-ml
+echo 'building bagend-ml'
+git pull
+docker build -t bagend-ml .
+
+cd ../event-store-api
+
+if [[ "$1" == '-r' ]]; then
+  echo 'restarting bagend.service'
+  systemctl restart bagend
+  fi
+
