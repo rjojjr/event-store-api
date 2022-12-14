494062377cd84eabef3829420da7cfbed987204c
kirchner <rjojjr91@live.com>
2022-12-14 11:53:26 -0600

    fix event store -rk

--- Program.cs
+++ Program.cs
@@ -16,7 +16,13 @@ builder.Services.AddSingleton<GenericEventService>();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressConsumesConstraintForFormFileParameters = true;
+        options.SuppressInferBindingSourcesForParameters = true;
+        options.SuppressModelStateInvalidFilter = true;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
